074a60cfb6ea7bded20c1a177309cdffc69cd788
Thomas Pecout <pecoutt@hotmail.fr>
2018-11-12 17:09:37 +0100

    symfony de ses morts

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -16,7 +16,25 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CategoryController extends AbstractController
 {
-
+    /**
+     * @Route("category", name="category")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index()
+    {
+        $categories = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findAll();
+        if (!$categories) {
+            throw $this->createNotFoundException(
+                'No article found in article\'s table.'
+            );
+        }
+        return $this->render(
+            '/category.html.twig',
+            ['categories' => $categories]
+        );
+    }
     /**
      * @Route("/category/{id}", name="category_show")
      * @param Category $category
@@ -24,6 +42,6 @@ class CategoryController extends AbstractController
      */
     public function show(Category $category) :Response
     {
-        return $this->render('category.html.twig', ['category'=>$category]);
+        return $this->render('showcategory.html.twig', ['category'=>$category]);
     }
 }
