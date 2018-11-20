ed4b8548761c264c79171c7d6d2ec73a6897fd1c
Thomas Pecout <pecoutt@hotmail.fr>
2018-11-20 10:44:47 +0100

    chez tati tatou

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -11,8 +11,10 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Entity\Category;
+use App\Form\CategoryType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class CategoryController extends AbstractController
@@ -21,7 +23,7 @@ class CategoryController extends AbstractController
      * @Route("category", name="category")
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(Request $request): Response
     {
         $categories = $this->getDoctrine()
             ->getRepository(Category::class)
@@ -31,9 +33,19 @@ class CategoryController extends AbstractController
                 'No article found in article\'s table.'
             );
         }
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+            header("Location:/category");
+
+        }
         return $this->render(
-            '/category.html.twig',
-            ['categories' => $categories]
+            'category.html.twig',
+            ['categories' => $categories, 'form'=>$form->createView()]
         );
     }
 
@@ -42,11 +54,11 @@ class CategoryController extends AbstractController
      * @param string $categoryName
      * @return Response
      */
-    public function showJs(string $categoryName) :Response
+    public function showJs(string $categoryName): Response
     {
         $category = $this->getDoctrine()
             ->getRepository(Category::class)
-            ->findOneBy(['name'=> $categoryName]);
+            ->findOneBy(['name' => $categoryName]);
         $articles = $this->getDoctrine()
             ->getRepository(Article::class)
             ->findBy(['category' => $category->getId()]);
@@ -57,17 +69,17 @@ class CategoryController extends AbstractController
      * @Route("/category/{category}", name="category_show")
      * @return Response
      */
-    public function showByCategory(string $category) :Response
+    public function showByCategory(string $category): Response
     {
         $category = $this->getDoctrine()
             ->getRepository(Category::class)
-            ->findOneBy(['name'=> $category]);
+            ->findOneBy(['name' => $category]);
         $articles = $this->getDoctrine()
             ->getRepository(Article::class)
-            ->findBy(['category' => $category->getId()], ['id' => 'DESC'], 3 , 0);
+            ->findBy(['category' => $category->getId()], ['id' => 'DESC'], 3, 0);
         return $this->render('showcategory.html.twig', [
-            'articles'=>$articles,
-            'category'=>$category,
+            'articles' => $articles,
+            'category' => $category,
         ]);
 
     }
